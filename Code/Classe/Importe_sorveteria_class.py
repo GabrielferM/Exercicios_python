ed9606f8999713c63bbb7f90a5ee98ab78afce47
GabrielferM <ferreiramarragabriel@gmail.com>
2024-11-30 11:41:01 -0300

    Atividades de import

---
+++ Code/Classe/Importe_sorveteria_class.py
@@ -0,0 +1,5 @@
+from class_sorveteria import Sorveteria_ice
+
+Sabores1 = 'Baunilha','Blue berry','Amoras'
+sorveteria = Sorveteria_ice('fome','mechido',Sabores1)
+sorveteria.mostrar_lista()
---
+++ Code/Classe/Importe_usuario_class.py
@@ -0,0 +1,4 @@
+import class_usuario
+
+eu = class_usuario.Adm('gabriel','marra','19')
+eu.privilegios.Mostrar_privilegios()
